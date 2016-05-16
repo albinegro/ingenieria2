da9c867b0ff7b8cd1c1690dd4d3ab0a9a86fda92
lchiacchio <you@example.com>
2016-05-16 16:03:36 -0300

    eliminando archivo de prueba

--- proyecto/hopedaje/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from home import views
-
-urlpatterns = [
-
-    url(r'^$', views.home, name='home'),
-    url(r'^close_popup/$', views.close_popup, name='close_popup'),
-]
